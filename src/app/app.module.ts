eb92c8a90e6dae139dc571d37e18f6ecd4b02398
NiponNumb <oropeza012@hotmail.com>
2023-11-27 17:18:27 -0600

    Terminado 1.0 para netifly

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,14 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { HashRouter } from 'react-router-dom';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PishingComponent } from 'src/crud/Pishing.component';
 import { PishingDetailComponent } from 'src/crud/Pishing-detail.component';
 import { GoogleComponent } from './google/google.component';
 import { GooglepasswordComponent } from './googlepassword/googlepassword.component';
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
  @NgModule({
   declarations: [
     AppComponent,
@@ -20,9 +22,9 @@ imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
   ],
-  providers: [],
+  providers: [{provide: LocationStrategy, useClass:HashLocationStrategy}],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
